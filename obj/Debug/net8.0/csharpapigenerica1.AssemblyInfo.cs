4db3e274e73bb7cfb9152c3a46c06afbcdc755df
samuelpai28 <gillopezsamuel65@gmail.com>
2025-03-06 16:00:48 -0500

    Actualización de configuración: agregado .vscode/, solución .sln e ignorados bin/ y obj/

--- obj/Debug/net8.0/csharpapigenerica1.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharpapigenerica1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharpapigenerica1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f663492b02800c0cc52003e070ac549a9a7c6544")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharpapigenerica1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharpapigenerica1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
